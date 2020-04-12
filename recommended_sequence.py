c697a4307d3f4e3dbe9cf2ce15dcdcbd341dbd92
Lhogeshwaran Purushothaman <lhogeshwaran.purushothaman@student.uts.edu.au>
2020-04-12 15:45:20 +1000

    Apple verified.

--- recommended_sequence.py
+++ recommended_sequence.py
@@ -12,38 +12,66 @@ F = {k: v for k, v in sorted(E.items(), key=lambda item: item[1], reverse=True)}
 S_ls = [x for x in S]
 
 for k, v in F.items():
+
+    S_ls = [x for x in S]
     tmp_W = [x for x in k]
 
+    tmp_hold = []
+    
     for i in tmp_W:
-
-#        if tmp_W.index(i) == 0:
 
             if i in S_ls:
                 tmp_S_ind = S_ls.index(i)
 
                 if tmp_S_ind != 0:
                     for j in range(tmp_S_ind):
+                        tmp_hold.append(S_ls[j])
+                        S_ls.remove(S_ls[j])
+                else:
+                    tmp_hold.append(S_ls[0])
+#                    S_ls.remove(S_ls[0])
+
+#                tmp_W.pop(tmp_W.index(i))
+
+
+for k, v in F.items():
+
+    S_ls = [x for x in S]
+    tmp_W = [x for x in k]
+    
+    tmp_S_ind = {}
+
+    for i in tmp_W:
+
+            if i in S_ls:
+                tmp_S_ind.setdefault(i, [])
+                tmp_S_ind[i].append(S_ls.index(i))
+
+                if tmp_S_ind != 0:
+                    for j in range(S_ls.index(i)):
                         S_ls.remove(S_ls[j])
                 else:
                     S_ls.remove(S_ls[0])
 
-            tmp_W.pop(tmp_W.index(i))
+#                tmp_W.pop(tmp_W.index(i))
 
 
+for k, v in F.items():
 
-                        try:
-                            S_ls.remove(S_ls[j])
-                        except:
-                            IndexError
-        
-        if tmp_W.index(i) != 0:
+    tmp_W = [x for x in k]
 
-            tmp_S_ind = 0
+    checked_indexes = []
 
+    for i in tmp_W:
+
+        if tmp_W.index(i) == 0:
             if i in S_ls:
-                tmp_S_ind = S_ls.index(i)
-            
-            if tmp_S_ind > 0:
-                for j in range(tmp_S_ind):
+                checked_indexes.append(S_ls.index(i))
 
-                
+        else:
+            if i in S_ls[checked_indexes[-1]+1:]:
+                checked_indexes.append(S_ls[checked_indexes[-1]+1:].index(i)+checked_indexes[-1]+1)
+
+        word = [S_ls[x] for x in checked_indexes]
+
+        print(word)
